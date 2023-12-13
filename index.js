b48d42e0ff255fd6e134bbcf1c1e63ae179b2f99
mukeshkumargiri523 <mukeshkumargiri523@gmail.com>
2023-12-13 22:15:12 +0530

    added build in backend

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const Razorpay = require("razorpay");
 const { app } = require("./app");
 
 exports.instance = new Razorpay({
-  // key_id: process.env.RAZORPAY_API_KEY,
-  // key_secret: process.env.RAZORPAY_SECRET,
-  key_id: "rzp_test_BB1Eq2vlfd3ODv",
-  key_secret: "OcpnuB6MN1gR7Ov5eeQpAy8X",
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_SECRET,
 });
 
 app.listen(process.env.PORT, () =>
